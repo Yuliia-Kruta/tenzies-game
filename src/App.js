a756db4ac6394130e2acebca09efae21d85fde90
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-03-19 10:06:25 +1000

    End of the game functionality added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Tile from "./components/Tile";
+import Confetti from 'react-confetti'
 
 
 function App() {
 
 const [diceNumbers, setDiceNumbers] = useState(newDices())
+const [game, setGame] = useState(false)
 
   function generateNewDice(){
     return {value: Math.floor(Math.random()*6)+1, isHeld: false}
@@ -19,9 +21,13 @@ const [diceNumbers, setDiceNumbers] = useState(newDices())
   }
 
   function rollDice(){
-    setDiceNumbers(prevDiceNumbers => prevDiceNumbers.map(tile => {
-      return tile.isHeld ? tile : generateNewDice()
-    }))
+    if (game){
+      setGame(false)
+      setDiceNumbers(newDices())
+    }else{
+      setDiceNumbers(prevDiceNumbers => prevDiceNumbers.map(tile => {
+        return tile.isHeld ? tile : generateNewDice()
+    }))}
   }
 
   function holdDice(tileId){
@@ -33,14 +39,25 @@ const [diceNumbers, setDiceNumbers] = useState(newDices())
   const tileElements = diceNumbers.map((diceNum, index) => {
     return <Tile key={index} value={diceNum.value} isHeld={diceNum.isHeld} holdDice={() => holdDice(index)}/>
   })
-  
+
+  useEffect(() => {
+    const areAllHeld = diceNumbers.every(tile => tile.isHeld)
+    const firstDice = diceNumbers[0].value
+    const areAllSame = diceNumbers.every(tile => tile.value === firstDice)
+    if(areAllHeld && areAllSame){
+      setGame(true)
+    }
+  }, [diceNumbers])
 
   return(
     <main>
+      {game && <Confetti />}
+       <h1 className="title">Tenzies</h1>
+          <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="tile-container">
         {tileElements}
       </div>
-      <button className="roll-button" onClick={rollDice}>Roll</button>
+      <button className="roll-button" onClick={rollDice}>{game ? "New game" : "Roll"}</button>
     </main>
   );
 }
